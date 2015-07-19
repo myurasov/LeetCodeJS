f4d742cefeb97b76f2d70e554d6352c2396da3ed
Mikhail Yurasov <me@yurasov.me>
2015-07-19 00:13:33 -0700

    #003

---
+++ 003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+
+	var h = new Set();
+	var m = 0;
+
+	for (var i = 0; i < s.length; i++) {
+		h.clear();
+		h.add(s.charAt(i));
+
+		var mm = 1;
+
+		for (j = i + 1; j < s.length; j++) {
+			var jc = s.charAt(j);
+
+			if (h.has(jc)) {
+				break;
+			} else {
+				mm++;
+				h.add(jc);
+			}
+		}
+
+		m = Math.max(m, mm);
+	}
+
+	return m;
+};
