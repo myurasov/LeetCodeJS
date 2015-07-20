74b96710d1a93d69915f852f21e43577779d822f
Mikhail Yurasov <me@yurasov.me>
2015-07-19 19:20:34 -0700

    #082

---
+++ 082-remove-duplicates-from-sorted-list-ii.js
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+Array.prototype.peek = function() {
+	return this.length > 0 ? this[this.length - 1] : undefined;
+};
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+	var r = [];
+	var a = new Set();
+
+	while (head) {
+		if (a.has(head.val)) {
+			// found duplicate
+			while (r.peek() == head.val) {
+				r.pop();
+			}
+		} else {
+			r.push(head.val);
+		}
+
+		a.add(head.val);
+		head = head.next;
+	}
+
+	return r;
+};
+
