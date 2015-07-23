46f502618197262c37c72a111ac217ee2308c223
Mikhail Yurasov <me@yurasov.me>
2015-07-22 18:23:52 -0700

    #154

---
+++ 154-find-minimum-in-rotated-sorted-array-ii.js
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
+
+// Can contain more minor optimizations, but essentially comes to 2 cases:
+// - array is sorted
+// - arrays is rotated and (as it contains duplicates) linear search is applicable
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function (nums) {
+	if (nums[0] < nums[nums.length - 1]) {
+		return nums[0];
+	}
+
+	return findMinLinear(nums);
+};
+
+var findMinLinear = function (nums) {
+	var i = nums.length - 1;
+	var min = nums[i];
+
+	while (i-- > 0) {
+		if (nums[i] < min) min = nums[i];
+	}
+
+	return min;
+}
