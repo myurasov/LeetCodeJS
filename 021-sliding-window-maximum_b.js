e44c6e5a9957141bc5f3a8d5c75805b56e044840
Mikhail Yurasov <me@yurasov.me>
2015-07-27 23:16:18 -0700

    Refactoring, comments

--- 021-sliding-window-maximum_b.js
+++ 021-sliding-window-maximum_b.js
@@ -1,13 +1,12 @@
 // https://leetcode.com/problems/sliding-window-maximum/
 
-// Double-ended queue
-function DEQueue() {}
-DEQueue.prototype = new Array();
-DEQueue.prototype.getLeft = function () { return this[0]; };
-DEQueue.prototype.getRight = function () { return this[this.length - 1]; };
-DEQueue.prototype.removeLeft = Array.prototype.shift;
-DEQueue.prototype.removeRight = Array.prototype.pop;
-DEQueue.prototype.pushRight = Array.prototype.push;
+function DoubleEndedQueue() {}
+DoubleEndedQueue.prototype = new Array();
+DoubleEndedQueue.prototype.getLeft = function () { return this[0]; };
+DoubleEndedQueue.prototype.getRight = function () { return this[this.length - 1]; };
+DoubleEndedQueue.prototype.removeLeft = Array.prototype.shift;
+DoubleEndedQueue.prototype.removeRight = Array.prototype.pop;
+DoubleEndedQueue.prototype.pushRight = Array.prototype.push;
 
 /**
  * @param {number[]} nums
@@ -15,20 +14,23 @@ DEQueue.prototype.pushRight = Array.prototype.push;
  * @return {number[]}
  */
 var maxSlidingWindow = function (nums, k) {
-	var w = new DEQueue();
+	var w = new DoubleEndedQueue();
 	var m = [];
 	var i = -1;
 
 	for (var a = 0; a < nums.length; a++) {
 
+		// remove elements not in the current window
 		while (w.length > 0 && w.getLeft() < (a - k + 1)) {
 			w.removeLeft();
 		}
 
+		// if previously added candidate is not current maximum, remove it
 		while (w.length > 0 && nums[w.getRight()] < nums[a]) {
 			w.removeRight();
 		}
 
+		// add "candidate" index
 		w.pushRight(a);
 
 		if (a >= k - 1) {
