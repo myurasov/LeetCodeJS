1827a4d746d20142813ef2a3765a9ef0c9c1fd0a
Mikhail Yurasov <me@yurasov.me>
2015-08-01 01:42:06 -0700

    Update #231

--- 231-power-of-two.js
+++ 231-power-of-two.js
@@ -5,10 +5,5 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function (n) {
-	while (n > 0 && (n & 1) === 0) {
-		n = n >> 1;
-	}
-
-	// power of 2 has only topmost bit equal to 1
-	return n === 1;
+	return (n > 0 && (n & (n - 1)) === 0);
 };
