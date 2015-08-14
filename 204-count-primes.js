2b295ed430aa816e4a7721b5e2d5758cddb1e69d
Mikhail Yurasov <me@yurasov.me>
2015-08-13 23:57:07 -0700

    #204

---
+++ 204-count-primes.js
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/count-primes/
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function (n) {
+	var res = 0;
+
+	while (--n > 1) {
+		if (isPrime(n)) {
+			res++;
+		}
+	}
+
+	return res;
+};
+
+var isPrime = function (n) {
+	for (var d = 2; d <= Math.sqrt(n); d++) {
+		if (n % d === 0) return false;
+	}
+
+	return true;
+}
